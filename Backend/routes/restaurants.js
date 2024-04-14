f4bd53b440d2c845ef237ddcd965f9e9928be74e
Star2578 <nuttykingger@gmail.com>
2024-04-14 19:27:48 +0700

    Add owner to restaurant schema

--- Backend/routes/restaurants.js
+++ Backend/routes/restaurants.js
@@ -11,8 +11,8 @@ const router =express.Router();
 
 router.use('/:restaurantId/reservations/',reservationRouter)
 
-router.route('/').get(getRestaurants).post(protect,authorize('admin'),createRestaurant);
-router.route('/:id').get(getRestaurant).put(protect,authorize('admin'),updateRestaurant).delete(protect,authorize('admin'),deleteRestaurant);
+router.route('/').get(getRestaurants).post(protect,authorize('admin', 'owner'),createRestaurant);
+router.route('/:id').get(getRestaurant).put(protect,authorize('admin', 'owner'),updateRestaurant).delete(protect,authorize('admin', 'owner'),deleteRestaurant);
 router.route('/:restaurantId/tables').get(getTables);
 
 
