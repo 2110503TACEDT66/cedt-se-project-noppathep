f1ee0c38e0ab83288ba50f16951bbae8a4ed6335
Star2578 <nuttykingger@gmail.com>
2024-04-25 21:36:14 +0700

    implement drawer panel

---
+++ Frontend/src/components/DrawerPanel.tsx
@@ -0,0 +1,37 @@
+"use client"
+import React, { useState } from 'react';
+import { Drawer, Button } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import CloseIcon from '@mui/icons-material/Close';
+
+export default function DrawerPanel ({children}:{children:React.ReactNode}) {
+
+  const [open, setOpen] = useState(false);
+  const toggleDrawerOpen = ()=> setOpen(!open)
+
+  return (
+    <>
+      <div className='w-fit mr-auto flex flex-row'>
+        <Button onClick={toggleDrawerOpen}>
+          <MenuIcon className='text-black' fontSize='large'/>
+        </Button>
+      </div>
+
+      <Drawer anchor="left" open={open} onClose={toggleDrawerOpen} sx={{
+        '& .MuiDrawer-paper': {
+          backgroundColor:"#101010",
+          width:'80%',
+        },
+      }}
+      >
+        <div className="flex flex-col justify-center first:items-start items-center py-2 gap-y-5">
+          <Button onClick={toggleDrawerOpen}>
+            <CloseIcon className='text-gray-100 ml-5' fontSize='large' />
+          </Button>
+          {children}
+        </div>
+      </Drawer>
+    </>
+  );
+};
--- Frontend/src/components/TopMenu.tsx
+++ Frontend/src/components/TopMenu.tsx
@@ -5,6 +5,15 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import { Link } from '@mui/material'
 import getUserProfile from '@/libs/user/getUserProfile'
+import { Hidden } from '@mui/material'
+import DrawerPanel from './DrawerPanel'
+
+import { Login, Logout } from '@mui/icons-material';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import SettingsIcon from '@mui/icons-material/Settings';
+import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
 
 export default async function TopMenu(){
 
@@ -17,44 +26,103 @@ export default async function TopMenu(){
 
     return(
         <div className={styles.menucontainer}>
-            {
-                session ? 
-                <Link href="/api/auth/signout">
-                    <div className='flex items-center h-full px-2 text-cyan-600 text-sm truncate '>Sign-out of {session.user?.name}</div>
-                </Link> 
-                : 
-                <Link href="/api/auth/signin">
-                    <div className='flex items-center h-full px-2 text-cyan-600 text-sm'>Sign-in</div>
-                </Link>
-            }
-            {
-                session?
-                <div className='flex items-center h-full px-2 text-cyan-600 text-sm'>
+            <Hidden smUp>
+                <DrawerPanel>
                     {
-                        profile.data.role == "user" && (
-                            <TopMenuItem title='My Reservation' pageRef='/myreservation'/>
-                        )
+                        session
+                            ?   <Link href={"/api/auth/signout"} className="w-full h-fit p-1 pl-3 pr-3 mt-auto mb-auto text-center text-gray-100
+                                    text-2xl hover:bg-slate-800" underline="none">
+                                    <div className="text-red-600  overflow-hidden"> <Logout fontSize="medium"/> &nbsp;Sign-Out 
+                                        <br/><span className="text-gray-400 text-base text-ellipsis">({session.user.name})</span>
+                                    </div>
+                                </Link>
+                            :   <div className="flex flex-col justify-center items-center w-full h-full gap-y-5">
+                                    <Link href={"/api/auth/signin"} className="w-full h-fit p-2 pl-3 pr-3 mt-auto mb-auto text-center text-gray-100
+                                    text-2xl hover:bg-slate-800 " underline="none">
+                                        <div className="text-gray-100 line-clamp-1"> <Login fontSize="medium" /> &nbsp;Sign-In</div>
+                                    </Link>
+                                    <Link href={"/api/auth/register"} className="w-full h-fit p-2 pl-3 pr-3 mt-auto mb-auto text-center text-gray-100
+                                    text-2xl hover:bg-slate-800 " underline="none">
+                                        <div className="text-gray-100 line-clamp-1"> <AssignmentIndIcon fontSize="medium" /> &nbsp;Register</div>
+                                    </Link>
+                                </div>
                     }
                     {
-                        profile.data.role === "owner" && (
-                            <TopMenuItem title='My Restaurant' pageRef='/myrestaurant'/>
+                        session && (
+                            <div className='flex flex-col justify-center items-center w-full gap-3'>
+                                {
+                                    profile.data.role == "user" && (
+                                        <Link href="/myreservation" underline="none" className="w-full text-center p-2 pl-3 pr-3 text-2xl hover:bg-slate-800 text-gray-100">
+                                            <div className="text-gray-100 line-clamp-1"><RestaurantIcon fontSize="medium" /> &nbsp;&nbsp;My Reservation</div>
+                                        </Link>
+                                    )
+                                }
+                                {
+                                    profile.data.role === "owner" && (
+                                        <Link href="/myrestaurant" underline="none" className="w-full text-center p-2 pl-3 pr-3 text-2xl hover:bg-slate-800 text-gray-100">
+                                            <div className="text-gray-100 line-clamp-1"><StorefrontIcon fontSize="medium" /> &nbsp;&nbsp;My Restaurant</div>
+                                        </Link>
+                                    )
+                                }
+                                {
+                                    profile.data.role === "admin" && (
+                                        <Link href="/manage" underline="none" className="w-full text-center p-2 pl-3 pr-3 text-2xl hover:bg-slate-800 text-gray-100">
+                                            <div className="text-gray-100 line-clamp-1"><SettingsIcon fontSize="medium" /> &nbsp;&nbsp;Manage</div>
+                                        </Link>
+                                    )
+                                }
+
+                                <Link href="/reservation" underline="none" className="w-full text-center p-2 pl-3 pr-3 text-2xl hover:bg-slate-800 text-gray-100">
+                                    <div className="text-gray-100 line-clamp-1"><RestaurantMenuIcon fontSize="medium" /> &nbsp;&nbsp;Reservation</div>
+                                </Link>
+                            </div>
                         )
                     }
-                    {
-                        profile.data.role === "admin" && (
-                            <TopMenuItem title='Manage' pageRef='/manage'/>
-                        )
-                    }
-                </div>
-                :
-                <Link href="/api/auth/register">
-                    <div className='flex items-center h-full px-2 text-cyan-600 text-sm'>Register</div>
-                </Link>
-            }
+                </DrawerPanel>
+            </Hidden>
             
-            <TopMenuItem title='Reservation' pageRef='/reservation'/>
+            <Hidden smDown>
+
+                {
+                    session ? 
+                    <Link href="/api/auth/signout">
+                        <div className='flex items-center h-full px-2 text-cyan-600 text-sm truncate '>Sign-out of {session.user?.name}</div>
+                    </Link> 
+                    : 
+                    <Link href="/api/auth/signin">
+                        <div className='flex items-center h-full px-2 text-cyan-600 text-sm'>Sign-in</div>
+                    </Link>
+                }
+                {
+                    session?
+                    <div className='flex items-center h-full px-2 text-cyan-600 text-sm'>
+                        {
+                            profile.data.role == "user" && (
+                                <TopMenuItem title='My Reservation' pageRef='/myreservation'/>
+                            )
+                        }
+                        {
+                            profile.data.role === "owner" && (
+                                <TopMenuItem title='My Restaurant' pageRef='/myrestaurant'/>
+                            )
+                        }
+                        {
+                            profile.data.role === "admin" && (
+                                <TopMenuItem title='Manage' pageRef='/manage'/>
+                            )
+                        }
+                    </div>
+                    :
+                    <Link href="/api/auth/register">
+                        <div className='flex items-center h-full px-2 text-cyan-600 text-sm'>Register</div>
+                    </Link>
+                }
+                
+                <TopMenuItem title='Reservation' pageRef='/reservation'/>
+            </Hidden>
+
             <Link href="/" className='ml-4'>
-            <Image src='/img/logo.jpg' className={styles.logoimg} alt='logo' width={0} height={0} sizes='100vh'/>
+                <Image src='/img/logo.jpg' className={styles.logoimg} alt='logo' width={0} height={0} sizes='100vh'/>
             </Link>
         </div>
     )
